58c5c652019cb05fe80db0c23f6bd6e0c7aaa413
Daniela Morais <dani.m@hotmail.co.uk>
2016-07-19 09:55:32 -0300

    Insert iamage and new activity

--- AndroidCourse_FirstExample/app/src/main/java/androidcourse/venturus/org/br/androidcourse_firstexample/MainActivity.java
+++ AndroidCourse_FirstExample/app/src/main/java/androidcourse/venturus/org/br/androidcourse_firstexample/MainActivity.java
@@ -1,11 +1,13 @@
 package androidcourse.venturus.org.br.androidcourse_firstexample;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,11 +22,11 @@ public class MainActivity extends AppCompatActivity {
         //save status and update resource
         mPrefs = getSharedPreferences("arquivo_preferencias", Context.MODE_PRIVATE);
         numberLikes = mPrefs.getInt("num_likes", 0);
-
         String stringResource = getResources().getString(R.string.likeCount, numberLikes);
         final TextView textLikes = (TextView) findViewById(R.id.countLikes);
         textLikes.setText(stringResource);
 
+        //update button on click
         final Button likeButton = (Button) findViewById(R.id.likeButton);
         likeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +36,17 @@ public class MainActivity extends AppCompatActivity {
                 textLikes.setText(stringResource);
             }
         });
+
+        //change activity
+        ImageView robotImageView = (ImageView) findViewById(R.id.marshmallow_image);
+        robotImageView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent pictureIntent = new Intent(MainActivity.this, PictureActivity.class);
+                MainActivity.this.startActivity(pictureIntent);
+            }
+        });
+
     }
 
     @Override
---
+++ AndroidCourse_FirstExample/app/src/main/java/androidcourse/venturus/org/br/androidcourse_firstexample/PictureActivity.java
@@ -0,0 +1,18 @@
+package androidcourse.venturus.org.br.androidcourse_firstexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vntlab on 7/19/16.
+ */
+public class PictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture);
+    }
+
+
+}
